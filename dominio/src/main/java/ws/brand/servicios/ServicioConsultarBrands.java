aad8e287ccf60dd19ed70ca62baa9cc6add6e1d2
Ricardo Roa <ricardo2roa@gmail.com>
2024-02-21 00:05:19 -0500

    Update ServicioConsultarBrands.java

--- dominio/src/main/java/ws/brand/servicios/ServicioConsultarBrands.java
+++ dominio/src/main/java/ws/brand/servicios/ServicioConsultarBrands.java
@@ -6,15 +6,17 @@ import org.springframework.data.domain.PageRequest;
 import ws.brand.modelo.entidad.Brand;
 import ws.brand.puerto.repositorio.RepositorioBrand;
 
+import java.util.List;
+
 public class ServicioConsultarBrands {
-    public static final int allBrands.size() = 10;
+
     private final RepositorioBrand repositorioBrand;
 
     public ServicioConsultarBrands(RepositorioBrand repositorioBrand) {
         this.repositorioBrand = repositorioBrand;
     }
     public Page<Brand> ejecutar(int numberPage, Boolean disabled, Boolean locked){
-        var allBrands = this.repositorioBrand.obtenerTodasLasMarcas(numberPage, disabled, locked);
+        List<Brand> allBrands = this.repositorioBrand.obtenerTodasLasMarcas(numberPage, disabled, locked);
         var size = this.repositorioBrand.calcularCode() - 1;
         return new PageImpl<>(
                 allBrands,
